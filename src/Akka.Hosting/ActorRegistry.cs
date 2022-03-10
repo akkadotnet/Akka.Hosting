50cf1e8fe7f5d4910de66e5b98c31109f9b81487
Aaron Stannard <aaron@petabridge.com>
2022-03-09 20:18:18 -0600

    migrated all libraries to .NET Standard 2.0

--- src/Akka.Hosting/ActorRegistry.cs
+++ src/Akka.Hosting/ActorRegistry.cs
@@ -3,110 +3,112 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using Akka.Actor;
 
-namespace Akka.Hosting;
-
-/// <summary>
-/// INTERNAL API
-/// </summary>
-public class ActorRegistryExtension : ExtensionIdProvider<ActorRegistry>
+namespace Akka.Hosting
 {
-    public override ActorRegistry CreateExtension(ExtendedActorSystem system)
-    {
-        return new ActorRegistry();
-    }
-}
-
-/// <summary>
-/// Mutable, but thread-safe <see cref="ActorRegistry"/>.
-/// </summary>
-/// <remarks>
-/// Should only be used for top-level actors that need to be accessed from inside or outside the <see cref="ActorSystem"/>.
-///
-/// If you are adding every single actor in your <see cref="ActorSystem"/> to the registry you are definitely using it wrong.
-/// </remarks>
-public class ActorRegistry : IExtension
-{
-    private readonly ConcurrentDictionary<Type, IActorRef> _actorRegistrations = new ConcurrentDictionary<Type, IActorRef>();
-
     /// <summary>
-    /// Attempts to register an actor with the registry.
+    /// INTERNAL API
     /// </summary>
-    /// <param name="actor">The bound <see cref="IActorRef"/>, if any. Is set to <see cref="ActorRefs.Nobody"/> if key is not found.</param>
-    /// <param name="overwrite">If <c>true</c>, allows overwriting of a previous actor with the same key. Defaults to <c>false</c>.</param>
-    /// <returns><c>true</c> if the actor was set to this key in the registry, <c>false</c> otherwise.</returns>
-    public bool TryRegister<TKey>(IActorRef actor, bool overwrite = false)
+    public class ActorRegistryExtension : ExtensionIdProvider<ActorRegistry>
     {
-        return TryRegister(typeof(TKey), actor, overwrite);
-    }
-    
-    /// <summary>
-    /// Attempts to register an actor with the registry.
-    /// </summary>
-    /// <param name="key">The key for a particular actor.</param>
-    /// <param name="actor">The bound <see cref="IActorRef"/>, if any. Is set to <see cref="ActorRefs.Nobody"/> if key is not found.</param>
-    /// <param name="overwrite">If <c>true</c>, allows overwriting of a previous actor with the same key. Defaults to <c>false</c>.</param>
-    /// <returns><c>true</c> if the actor was set to this key in the registry, <c>false</c> otherwise.</returns>
-    public bool TryRegister(Type key, IActorRef actor, bool overwrite = false)
-    {
-        if (!overwrite)
-            return _actorRegistrations.TryAdd(key, actor);
-        else
-            _actorRegistrations[key] = actor;
-        return true;
-    }
-    
-    /// <summary>
-    /// Try to retrieve an <see cref="IActorRef"/> with the given <see cref="TKey"/>.
-    /// </summary>
-    /// <param name="actor">The bound <see cref="IActorRef"/>, if any. Is set to <see cref="ActorRefs.Nobody"/> if key is not found.</param>
-    /// <returns><c>true</c> if an actor with this key exists, <c>false</c> otherwise.</returns>
-    public bool TryGet<TKey>(out IActorRef actor)
-    {
-        return TryGet(typeof(TKey), out actor);
-    }
-
-    /// <summary>
-    /// Try to retrieve an <see cref="IActorRef"/> with the given <see cref="TKey"/>.
-    /// </summary>
-    /// <param name="key">The key for a particular actor.</param>
-    /// <param name="actor">The bound <see cref="IActorRef"/>, if any. Is set to <see cref="ActorRefs.Nobody"/> if key is not found.</param>
-    /// <returns><c>true</c> if an actor with this key exists, <c>false</c> otherwise.</returns>
-    public bool TryGet(Type key, out IActorRef actor)
-    {
-        if (_actorRegistrations.ContainsKey(key))
+        public override ActorRegistry CreateExtension(ExtendedActorSystem system)
         {
-            actor = _actorRegistrations[key];
+            return new ActorRegistry();
+        }
+    }
+
+    /// <summary>
+    /// Mutable, but thread-safe <see cref="ActorRegistry"/>.
+    /// </summary>
+    /// <remarks>
+    /// Should only be used for top-level actors that need to be accessed from inside or outside the <see cref="ActorSystem"/>.
+    ///
+    /// If you are adding every single actor in your <see cref="ActorSystem"/> to the registry you are definitely using it wrong.
+    /// </remarks>
+    public class ActorRegistry : IExtension
+    {
+        private readonly ConcurrentDictionary<Type, IActorRef> _actorRegistrations =
+            new ConcurrentDictionary<Type, IActorRef>();
+
+        /// <summary>
+        /// Attempts to register an actor with the registry.
+        /// </summary>
+        /// <param name="actor">The bound <see cref="IActorRef"/>, if any. Is set to <see cref="ActorRefs.Nobody"/> if key is not found.</param>
+        /// <param name="overwrite">If <c>true</c>, allows overwriting of a previous actor with the same key. Defaults to <c>false</c>.</param>
+        /// <returns><c>true</c> if the actor was set to this key in the registry, <c>false</c> otherwise.</returns>
+        public bool TryRegister<TKey>(IActorRef actor, bool overwrite = false)
+        {
+            return TryRegister(typeof(TKey), actor, overwrite);
+        }
+
+        /// <summary>
+        /// Attempts to register an actor with the registry.
+        /// </summary>
+        /// <param name="key">The key for a particular actor.</param>
+        /// <param name="actor">The bound <see cref="IActorRef"/>, if any. Is set to <see cref="ActorRefs.Nobody"/> if key is not found.</param>
+        /// <param name="overwrite">If <c>true</c>, allows overwriting of a previous actor with the same key. Defaults to <c>false</c>.</param>
+        /// <returns><c>true</c> if the actor was set to this key in the registry, <c>false</c> otherwise.</returns>
+        public bool TryRegister(Type key, IActorRef actor, bool overwrite = false)
+        {
+            if (!overwrite)
+                return _actorRegistrations.TryAdd(key, actor);
+            else
+                _actorRegistrations[key] = actor;
             return true;
         }
 
-        actor = ActorRefs.Nobody;
-        return false;
-    }
+        /// <summary>
+        /// Try to retrieve an <see cref="IActorRef"/> with the given <see cref="TKey"/>.
+        /// </summary>
+        /// <param name="actor">The bound <see cref="IActorRef"/>, if any. Is set to <see cref="ActorRefs.Nobody"/> if key is not found.</param>
+        /// <returns><c>true</c> if an actor with this key exists, <c>false</c> otherwise.</returns>
+        public bool TryGet<TKey>(out IActorRef actor)
+        {
+            return TryGet(typeof(TKey), out actor);
+        }
 
-    /// <summary>
-    /// Fetches the <see cref="IActorRef"/> by key.
-    /// </summary>
-    /// <typeparam name="TKey">The key type to retrieve this actor.</typeparam>
-    /// <returns>If found, the underlying <see cref="IActorRef"/>.
-    /// If not found, returns <see cref="ActorRefs.Nobody"/>.</returns>
-    public IActorRef Get<TKey>()
-    {
-        if (TryGet<TKey>(out var actor))
-            return actor;
-        return ActorRefs.Nobody;
-    }
+        /// <summary>
+        /// Try to retrieve an <see cref="IActorRef"/> with the given <see cref="TKey"/>.
+        /// </summary>
+        /// <param name="key">The key for a particular actor.</param>
+        /// <param name="actor">The bound <see cref="IActorRef"/>, if any. Is set to <see cref="ActorRefs.Nobody"/> if key is not found.</param>
+        /// <returns><c>true</c> if an actor with this key exists, <c>false</c> otherwise.</returns>
+        public bool TryGet(Type key, out IActorRef actor)
+        {
+            if (_actorRegistrations.ContainsKey(key))
+            {
+                actor = _actorRegistrations[key];
+                return true;
+            }
 
-    /// <summary>
-    /// Allows enumerated access to the collection of all registered actors.
-    /// </summary>
-    /// <returns></returns>
-    public IEnumerator<KeyValuePair<Type, IActorRef>> GetEnumerator()
-    {
-        return _actorRegistrations.GetEnumerator();
-    }
+            actor = ActorRefs.Nobody;
+            return false;
+        }
 
-    public static ActorRegistry For(ActorSystem actorSystem)
-    {
-        return actorSystem.WithExtension<ActorRegistry, ActorRegistryExtension>();
+        /// <summary>
+        /// Fetches the <see cref="IActorRef"/> by key.
+        /// </summary>
+        /// <typeparam name="TKey">The key type to retrieve this actor.</typeparam>
+        /// <returns>If found, the underlying <see cref="IActorRef"/>.
+        /// If not found, returns <see cref="ActorRefs.Nobody"/>.</returns>
+        public IActorRef Get<TKey>()
+        {
+            if (TryGet<TKey>(out var actor))
+                return actor;
+            return ActorRefs.Nobody;
+        }
+
+        /// <summary>
+        /// Allows enumerated access to the collection of all registered actors.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<KeyValuePair<Type, IActorRef>> GetEnumerator()
+        {
+            return _actorRegistrations.GetEnumerator();
+        }
+
+        public static ActorRegistry For(ActorSystem actorSystem)
+        {
+            return actorSystem.WithExtension<ActorRegistry, ActorRegistryExtension>();
+        }
     }
 }
--- src/Akka.Hosting/AkkaConfigurationBuilder.cs
+++ src/Akka.Hosting/AkkaConfigurationBuilder.cs
@@ -8,229 +8,230 @@ using Akka.DependencyInjection;
 using Akka.Util;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Akka.Hosting;
-
-/// <summary>
-/// Delegate used to configure how to merge new HOCON in with the previous HOCON
-/// that has already been added to the <see cref="AkkaConfigurationBuilder"/>.
-/// </summary>
-public delegate Config HoconConfigurator(Config currentConfig, Config configToAdd);
-
-/// <summary>
-/// Describes how to add a new <see cref="Config"/> section to our existing HOCON.
-/// </summary>
-public enum HoconAddMode
+namespace Akka.Hosting
 {
     /// <summary>
-    /// Appends this HOCON to the back as a fallback.
+    /// Delegate used to configure how to merge new HOCON in with the previous HOCON
+    /// that has already been added to the <see cref="AkkaConfigurationBuilder"/>.
     /// </summary>
-    Append,
+    public delegate Config HoconConfigurator(Config currentConfig, Config configToAdd);
 
     /// <summary>
-    /// Prepend this HOCON to the front, overriding current values without deleting them.
+    /// Describes how to add a new <see cref="Config"/> section to our existing HOCON.
     /// </summary>
-    Prepend,
-
-    /// <summary>
-    /// Replace all current HOCON with this HOCON instead.
-    /// </summary>
-    /// <remarks>
-    /// WARNING: this is a destructive action. If you are writing a plugin or extension, never
-    /// call a method with this value directly. Always allow the user to choose. 
-    /// </remarks>
-    Replace
-}
-
-/// <summary>
-/// Delegate used to instantiate <see cref="IActorRef"/>s once the <see cref="ActorSystem"/> has booted.
-/// </summary>
-public delegate Task ActorStarter(ActorSystem system, ActorRegistry registry);
-
-public sealed class AkkaConfigurationBuilder
-{
-    private readonly string _actorSystemName;
-    private readonly IServiceCollection _serviceCollection;
-    private readonly HashSet<Setup> _setups = new HashSet<Setup>();
-
-    /// <summary>
-    /// The currently configured <see cref="ProviderSelection"/>.
-    /// </summary>
-    public Option<ProviderSelection> ActorRefProvider { get; private set; } = Option<ProviderSelection>.None;
-
-    /// <summary>
-    /// The current HOCON configuration.
-    /// </summary>
-    public Option<Config> Configuration { get; private set; } = Option<Config>.None;
-    
-    /// <summary>
-    /// INTERNAL API.
-    ///
-    /// Used to hold a reference to the <see cref="Sys"/> being started.
-    /// </summary>
-    internal Option<ActorSystem> Sys { get; set; } = Option<ActorSystem>.None;
-
-    private readonly HashSet<ActorStarter> _actorStarters = new HashSet<ActorStarter>();
-    private bool _complete = false;
-
-    public AkkaConfigurationBuilder(IServiceCollection serviceCollection, string actorSystemName)
+    public enum HoconAddMode
     {
-        _serviceCollection = serviceCollection;
-        _actorSystemName = actorSystemName;
+        /// <summary>
+        /// Appends this HOCON to the back as a fallback.
+        /// </summary>
+        Append,
+
+        /// <summary>
+        /// Prepend this HOCON to the front, overriding current values without deleting them.
+        /// </summary>
+        Prepend,
+
+        /// <summary>
+        /// Replace all current HOCON with this HOCON instead.
+        /// </summary>
+        /// <remarks>
+        /// WARNING: this is a destructive action. If you are writing a plugin or extension, never
+        /// call a method with this value directly. Always allow the user to choose. 
+        /// </remarks>
+        Replace
     }
 
-    internal AkkaConfigurationBuilder AddSetup(Setup setup)
-    {
-        if (_complete) return this;
+    /// <summary>
+    /// Delegate used to instantiate <see cref="IActorRef"/>s once the <see cref="ActorSystem"/> has booted.
+    /// </summary>
+    public delegate Task ActorStarter(ActorSystem system, ActorRegistry registry);
 
-        // we will recreate our own BootstrapSetup later - just extract the parts for now.
-        if (setup is BootstrapSetup bootstrapSetup)
+    public sealed class AkkaConfigurationBuilder
+    {
+        private readonly string _actorSystemName;
+        private readonly IServiceCollection _serviceCollection;
+        private readonly HashSet<Setup> _setups = new HashSet<Setup>();
+
+        /// <summary>
+        /// The currently configured <see cref="ProviderSelection"/>.
+        /// </summary>
+        public Option<ProviderSelection> ActorRefProvider { get; private set; } = Option<ProviderSelection>.None;
+
+        /// <summary>
+        /// The current HOCON configuration.
+        /// </summary>
+        public Option<Config> Configuration { get; private set; } = Option<Config>.None;
+
+        /// <summary>
+        /// INTERNAL API.
+        ///
+        /// Used to hold a reference to the <see cref="Sys"/> being started.
+        /// </summary>
+        internal Option<ActorSystem> Sys { get; set; } = Option<ActorSystem>.None;
+
+        private readonly HashSet<ActorStarter> _actorStarters = new HashSet<ActorStarter>();
+        private bool _complete = false;
+
+        public AkkaConfigurationBuilder(IServiceCollection serviceCollection, string actorSystemName)
         {
-            if (bootstrapSetup.Config.HasValue)
-                Configuration = Configuration.HasValue
-                    ? Configuration.FlatSelect<Config>(c => bootstrapSetup.Config.Value.WithFallback(c))
-                    : bootstrapSetup.Config;
-            ActorRefProvider = bootstrapSetup.ActorRefProvider;
+            _serviceCollection = serviceCollection;
+            _actorSystemName = actorSystemName;
+        }
+
+        internal AkkaConfigurationBuilder AddSetup(Setup setup)
+        {
+            if (_complete) return this;
+
+            // we will recreate our own BootstrapSetup later - just extract the parts for now.
+            if (setup is BootstrapSetup bootstrapSetup)
+            {
+                if (bootstrapSetup.Config.HasValue)
+                    Configuration = Configuration.HasValue
+                        ? Configuration.FlatSelect<Config>(c => bootstrapSetup.Config.Value.WithFallback(c))
+                        : bootstrapSetup.Config;
+                ActorRefProvider = bootstrapSetup.ActorRefProvider;
+                return this;
+            }
+
+            // don't apply the diSetup
+            if (setup is DependencyResolverSetup)
+            {
+                return this;
+            }
+
+            _setups.Add(setup);
             return this;
         }
 
-        // don't apply the diSetup
-        if (setup is DependencyResolverSetup)
+        internal AkkaConfigurationBuilder WithActorRefProvider(ProviderSelection provider)
         {
+            if (provider == null)
+                throw new ArgumentNullException(nameof(provider));
+            if (_complete) return this;
+            ActorRefProvider = provider;
             return this;
         }
 
-        _setups.Add(setup);
-        return this;
-    }
-
-    internal AkkaConfigurationBuilder WithActorRefProvider(ProviderSelection provider)
-    {
-        if (provider == null)
-            throw new ArgumentNullException(nameof(provider));
-        if (_complete) return this;
-        ActorRefProvider = provider;
-        return this;
-    }
-
-    internal AkkaConfigurationBuilder AddHoconConfiguration(HoconConfigurator configurator, Config newHocon)
-    {
-        if (newHocon == null)
-            throw new ArgumentNullException(nameof(newHocon));
-        if (configurator == null)
-            throw new ArgumentNullException(nameof(configurator));
-
-        if (_complete) return this;
-        Configuration = configurator(Configuration.GetOrElse(Config.Empty), newHocon);
-        return this;
-    }
-
-    internal AkkaConfigurationBuilder AddHoconConfiguration(Config newHocon,
-        HoconAddMode addMode = HoconAddMode.Append)
-    {
-        switch (addMode)
+        internal AkkaConfigurationBuilder AddHoconConfiguration(HoconConfigurator configurator, Config newHocon)
         {
-            case HoconAddMode.Append:
-                return AddHoconConfiguration((config, add) => config.WithFallback(add), newHocon);
-            case HoconAddMode.Prepend:
-                return AddHoconConfiguration((config, add) => add.WithFallback(config), newHocon);
-            case HoconAddMode.Replace:
-                return AddHoconConfiguration((config, add) => add.WithFallback(config), newHocon);
-            default:
-                throw new ArgumentOutOfRangeException(nameof(addMode), addMode, null);
-        }
-    }
+            if (newHocon == null)
+                throw new ArgumentNullException(nameof(newHocon));
+            if (configurator == null)
+                throw new ArgumentNullException(nameof(configurator));
 
-    private static ActorStarter ToAsyncStarter(Action<ActorSystem, ActorRegistry> nonAsyncStarter)
-    {
-        Task Starter(ActorSystem f, ActorRegistry registry)
-        {
-            nonAsyncStarter(f, registry);
-            return Task.CompletedTask;
+            if (_complete) return this;
+            Configuration = configurator(Configuration.GetOrElse(Config.Empty), newHocon);
+            return this;
         }
 
-        return Starter;
-    }
-
-    public AkkaConfigurationBuilder StartActors(Action<ActorSystem, ActorRegistry> starter)
-    {
-        if (_complete) return this;
-        _actorStarters.Add(ToAsyncStarter(starter));
-        return this;
-    }
-
-    public AkkaConfigurationBuilder StartActors(ActorStarter starter)
-    {
-        if (_complete) return this;
-        _actorStarters.Add(starter);
-        return this;
-    }
-
-    internal void Build()
-    {
-        if (!_complete)
+        internal AkkaConfigurationBuilder AddHoconConfiguration(Config newHocon,
+            HoconAddMode addMode = HoconAddMode.Append)
         {
-            _complete = true;
-            _serviceCollection.AddSingleton<AkkaConfigurationBuilder>(this);
-
-            // start the IHostedService which will run Akka.NET
-            _serviceCollection.AddHostedService<AkkaHostedService>();
-
-            /*
-        * Build setups
-        */
-            var sp = _serviceCollection.BuildServiceProvider();
-            var diSetup = DependencyResolverSetup.Create(sp);
-            var bootstrapSetup = BootstrapSetup.Create().WithConfig(Configuration.GetOrElse(Config.Empty));
-            if (ActorRefProvider.HasValue) // only set the provider when explicitly required
+            switch (addMode)
             {
-                bootstrapSetup = bootstrapSetup.WithActorRefProvider(ActorRefProvider.Value);
+                case HoconAddMode.Append:
+                    return AddHoconConfiguration((config, add) => config.WithFallback(add), newHocon);
+                case HoconAddMode.Prepend:
+                    return AddHoconConfiguration((config, add) => add.WithFallback(config), newHocon);
+                case HoconAddMode.Replace:
+                    return AddHoconConfiguration((config, add) => add.WithFallback(config), newHocon);
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(addMode), addMode, null);
+            }
+        }
+
+        private static ActorStarter ToAsyncStarter(Action<ActorSystem, ActorRegistry> nonAsyncStarter)
+        {
+            Task Starter(ActorSystem f, ActorRegistry registry)
+            {
+                nonAsyncStarter(f, registry);
+                return Task.CompletedTask;
             }
 
-            var actorSystemSetup = bootstrapSetup.And(diSetup);
-            foreach (var setup in _setups)
+            return Starter;
+        }
+
+        public AkkaConfigurationBuilder StartActors(Action<ActorSystem, ActorRegistry> starter)
+        {
+            if (_complete) return this;
+            _actorStarters.Add(ToAsyncStarter(starter));
+            return this;
+        }
+
+        public AkkaConfigurationBuilder StartActors(ActorStarter starter)
+        {
+            if (_complete) return this;
+            _actorStarters.Add(starter);
+            return this;
+        }
+
+        internal void Build()
+        {
+            if (!_complete)
             {
-                actorSystemSetup = actorSystemSetup.And(setup);
+                _complete = true;
+                _serviceCollection.AddSingleton<AkkaConfigurationBuilder>(this);
+
+                // start the IHostedService which will run Akka.NET
+                _serviceCollection.AddHostedService<AkkaHostedService>();
+
+                /*
+            * Build setups
+            */
+                var sp = _serviceCollection.BuildServiceProvider();
+                var diSetup = DependencyResolverSetup.Create(sp);
+                var bootstrapSetup = BootstrapSetup.Create().WithConfig(Configuration.GetOrElse(Config.Empty));
+                if (ActorRefProvider.HasValue) // only set the provider when explicitly required
+                {
+                    bootstrapSetup = bootstrapSetup.WithActorRefProvider(ActorRefProvider.Value);
+                }
+
+                var actorSystemSetup = bootstrapSetup.And(diSetup);
+                foreach (var setup in _setups)
+                {
+                    actorSystemSetup = actorSystemSetup.And(setup);
+                }
+
+                /*
+                 * Start ActorSystem
+                 */
+                var sys = ActorSystem.Create(_actorSystemName, actorSystemSetup);
+
+                Sys = sys;
+
+                // register as singleton - not interested in supporting multi-Sys use cases
+                _serviceCollection.AddSingleton<ActorSystem>(sys);
+
+                var actorRegistry = ActorRegistry.For(sys);
+                _serviceCollection.AddSingleton(actorRegistry);
             }
+        }
+
+        internal Task<ActorSystem> StartAsync()
+        {
+            if (!_complete)
+                throw new InvalidOperationException("Cannot start Sys - Builder is not marked as complete.");
+
+            return StartAsync(Sys.Value);
+        }
+
+        internal async Task<ActorSystem> StartAsync(ActorSystem sys)
+        {
+            if (!_complete)
+                throw new InvalidOperationException("Cannot start Sys - Builder is not marked as complete.");
 
             /*
-             * Start ActorSystem
+             * Start Actors
              */
-            var sys = ActorSystem.Create(_actorSystemName, actorSystemSetup);
 
-            Sys = sys; 
+            var registry = ActorRegistry.For(sys);
 
-            // register as singleton - not interested in supporting multi-Sys use cases
-            _serviceCollection.AddSingleton<ActorSystem>(sys);
+            foreach (var starter in _actorStarters)
+            {
+                await starter(sys, registry).ConfigureAwait(false);
+            }
 
-            var actorRegistry = ActorRegistry.For(sys);
-            _serviceCollection.AddSingleton(actorRegistry);
+            return sys;
         }
-    }
-    
-    internal Task<ActorSystem> StartAsync()
-    {
-        if (!_complete)
-            throw new InvalidOperationException("Cannot start Sys - Builder is not marked as complete.");
-
-        return StartAsync(Sys.Value);
-    }
-
-    internal async Task<ActorSystem> StartAsync(ActorSystem sys)
-    {
-        if (!_complete)
-            throw new InvalidOperationException("Cannot start Sys - Builder is not marked as complete.");
-
-        /*
-         * Start Actors
-         */
-        
-        var registry = ActorRegistry.For(sys);
-        
-        foreach (var starter in _actorStarters)
-        {
-            await starter(sys, registry).ConfigureAwait(false);
-        }
-
-        return sys;
     }
 }
