06ef4f4278c89fff844c1d3b525ed6fd807272be
Aaron Stannard <aaron@petabridge.com>
2022-06-15 21:27:56 -0500

    fixed up Akka.Persistence.Hosting

--- src/Akka.Persistence.Hosting/AkkaPersistenceHostingExtensions.cs
+++ src/Akka.Persistence.Hosting/AkkaPersistenceHostingExtensions.cs
@@ -1,37 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using Akka.Configuration;
 using Akka.Hosting;
 using Akka.Persistence.Journal;
+using Akka.Util;
 
 namespace Akka.Persistence.Hosting
 {
+    /// <summary>
+    /// Used to help build journal configurations
+    /// </summary>
+    public sealed class AkkaPersistenceJournalBuilder
+    {
+        internal readonly string JournalId;
+        internal readonly AkkaConfigurationBuilder Builder;
+        internal readonly Dictionary<Type, HashSet<string>> Bindings = new Dictionary<Type, HashSet<string>>();
+        internal readonly Dictionary<string, Type> Adapters = new Dictionary<string, Type>();
+
+        public AkkaPersistenceJournalBuilder(string journalId, AkkaConfigurationBuilder builder)
+        {
+            JournalId = journalId;
+            Builder = builder;
+        }
+
+        public AkkaPersistenceJournalBuilder AddEventAdapter<TAdapter>(string eventAdapterName,
+            IEnumerable<Type> boundTypes) where TAdapter : IEventAdapter
+        {
+            Adapters[eventAdapterName] = typeof(TAdapter);
+            foreach (var t in boundTypes)
+            {
+                if (!Bindings.ContainsKey(t))
+                    Bindings[t] = new HashSet<string>();
+                Bindings[t].Add(eventAdapterName);
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// INTERNAL API - Builds the HOCON and then injects it.
+        /// </summary>
+        internal void Build()
+        {
+            // useless configuration - don't bother.
+            if (Adapters.Count == 0 || Bindings.Count == 0)
+                return;
+            
+            var adapters = new StringBuilder()
+                .Append($"akka.persistence.journal.{JournalId}").Append("{")
+                    .AppendLine("event-adapters {");
+            foreach (var kv in Adapters)
+            {
+                adapters.AppendLine($"{kv.Key} = \"{kv.Value.TypeQualifiedName()}\"");
+            }
+
+            adapters.AppendLine("}").AppendLine("event-adapter-bindings {");
+            foreach (var kv in Bindings)
+            {
+                adapters.AppendLine($"\"{kv.Key.TypeQualifiedName()}\" = [{string.Join(",", kv.Value)}]");
+            }
+
+            adapters.AppendLine("}").AppendLine("}");
+
+            var finalHocon = ConfigurationFactory.ParseString(adapters.ToString());
+            Builder.AddHocon(finalHocon, HoconAddMode.Prepend);
+        }
+    }
+    
     /// <summary>
     /// The set of options for generic Akka.Persistence.
     /// </summary>
     public static class AkkaPersistenceHostingExtensions
     {
         /// <summary>
-        /// Adds an <see cref="IEventAdapter"/> to a specific `akka.persistence.journal`.
+        /// Used to configure a specific Akka.Persistence.Journal instance, primarily to support <see cref="IEventAdapter"/>s.
         /// </summary>
         /// <param name="builder">The builder instance being configured.</param>
         /// <param name="journalId">The id of the journal. i.e. if you want to apply this adapter to the `akka.persistence.journal.sql` journal, just type `sql`.</param>
-        /// <param name="eventAdapterName">The short-hand name for this event-adapter. Needed for populating bindings.</param>
-        /// <param name="boundTypes">The set of event types that should be handled by <see cref="TAdapter"/>.</param>
-        /// <typeparam name="TAdapter">The type of <see cref="IEventAdapter"/> we want to bind to the journal.</typeparam>
+        /// <param name="journalBuilder">Configuration method for configuring the journal.</param>
         /// <returns>The same <see cref="AkkaConfigurationBuilder"/> instance originally passed in.</returns>
         /// <remarks>
         /// This method can be called multiple times for different <see cref="IEventAdapter"/>s.
         /// </remarks>
-        public static AkkaConfigurationBuilder AddJournalEventAdapter<TAdapter>(this AkkaConfigurationBuilder builder,
-            string journalId, string eventAdapterName, IEnumerable<Type> boundTypes) where TAdapter:IEventAdapter
+        public static AkkaConfigurationBuilder WithJournal(this AkkaConfigurationBuilder builder,
+            string journalId, Action<AkkaPersistenceJournalBuilder> journalBuilder)
         {
-            Config adapterConfiguration = @$"
-            akka.persistence.journal {{
-                {journalId} {{
-                   
-                }}
-            }}";
+            var jBuilder = new AkkaPersistenceJournalBuilder(journalId, builder);
+            journalBuilder(jBuilder);
+            
+            // build and inject the HOCON
+            jBuilder.Build();
+            return builder;
         }
     }
 }
