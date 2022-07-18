c969ceb27d846eae3d49cec40e173fb49802c338
Gregorius Soedharmo <arkatufus@yahoo.com>
2022-07-18 19:17:59 +0700

    Add ILoggerFactory logger support (#72)
    * Add ILoggerFactory logger support
    
    * Rename WithLoggerFactorLogger to WithLoggerFactory
    
    * Add ILoggerFactory sample project
    
    * Make `Akka.Hosting.LoggingDemo.csproj` not packable
    
    Co-authored-by: Aaron Stannard <aaron@petabridge.com>

---
+++ src/Examples/Akka.Hosting.LoggingDemo/Program.cs
@@ -0,0 +1,43 @@
+using Akka.Hosting;
+using Akka.Actor;
+using Akka.Actor.Dsl;
+using Akka.Cluster.Hosting;
+using Akka.Event;
+using Akka.Hosting.Logging;
+using Akka.Hosting.LoggingDemo;
+using Akka.Remote.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAkka("MyActorSystem", (configurationBuilder, serviceProvider) =>
+{
+    configurationBuilder
+        .AddHocon("akka.loglevel = DEBUG")
+        .WithLoggerFactory()
+        .WithRemoting("localhost", 8110)
+        .WithClustering(new ClusterOptions(){ Roles = new[]{ "myRole" }, 
+            SeedNodes = new[]{ Address.Parse("akka.tcp://MyActorSystem@localhost:8110")}})
+        .WithActors((system, registry) =>
+        {
+            var echo = system.ActorOf(act =>
+            {
+                act.ReceiveAny((o, context) =>
+                {
+                    Logging.GetLogger(context.System, "echo").Info($"Actor received {o}");
+                    context.Sender.Tell($"{context.Self} rcv {o}");
+                });
+            }, "echo");
+            registry.TryRegister<Echo>(echo); // register for DI
+        });
+});
+
+var app = builder.Build();
+
+app.MapGet("/", async (context) =>
+{
+    var echo = context.RequestServices.GetRequiredService<ActorRegistry>().Get<Echo>();
+    var body = await echo.Ask<string>(context.TraceIdentifier, context.RequestAborted).ConfigureAwait(false);
+    await context.Response.WriteAsync(body);
+});
+
+app.Run();
