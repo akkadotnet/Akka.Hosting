597f0ca7153dafdcd6bf378d56f1532882cc28cf
Gregorius Soedharmo <arkatufus@yahoo.com>
2023-02-16 23:13:00 +0700

    Update PR validation script

--- build/Build.CI.GitHubActions.cs
+++ build/Build.CI.GitHubActions.cs
@@ -13,8 +13,8 @@ using Nuke.Common.Utilities;
     GitHubActionsImage.WindowsLatest,
     GitHubActionsImage.UbuntuLatest,
     AutoGenerate = false,
-    OnPushBranches = new[] { "master", "dev" },
-    OnPullRequestBranches = new[] { "master", "dev" },
+    OnPushBranches = new[] { "master", "dev", "v1.4" },
+    OnPullRequestBranches = new[] { "master", "dev", "v1.4" },
     InvokedTargets = new[] { nameof(All) },
     PublishArtifacts = true,
     EnableGitHubToken = true)]
