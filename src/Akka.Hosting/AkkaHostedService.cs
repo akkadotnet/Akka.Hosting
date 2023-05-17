c57287fdf89e6f7e3c978aee6d4f79114636e30f
Aaron Stannard <aaron@petabridge.com>
2023-05-17 11:10:23 -0500

    Revert "Make `AkkaHostedService` public (#299)" (#305)
    This reverts commit 4724638b61b5d15a20a7be1bdacf06d7adc303f9.

--- src/Akka.Hosting/AkkaHostedService.cs
+++ src/Akka.Hosting/AkkaHostedService.cs
@@ -8,109 +8,66 @@ using Microsoft.Extensions.Logging;
 
 namespace Akka.Hosting
 {
-    public abstract class AkkaHostedService : IHostedService
-    {
-        private ActorSystem? _actorSystem;
-        private CoordinatedShutdown? _coordinatedShutdown; // grab a reference to CoordinatedShutdown early
-        
-        protected IServiceProvider ServiceProvider { get; }
-        protected IHostApplicationLifetime HostApplicationLifetime { get; }
-        protected ILogger<AkkaHostedService> Logger { get; }
-        
-        private readonly AkkaConfigurationBuilder _configurationBuilder;
-
-        protected AkkaHostedService(AkkaConfigurationBuilder configurationBuilder, IServiceProvider serviceProvider,
-            ILogger<AkkaHostedService> logger, IHostApplicationLifetime applicationLifetime)
-        {
-            _configurationBuilder = configurationBuilder;
-            HostApplicationLifetime = applicationLifetime;
-            ServiceProvider = serviceProvider;
-            Logger = logger;
-        }
-        
-        protected ActorSystem ActorSystem
-        {
-            get
-            {
-                if (_actorSystem is null)
-                    throw new Exception("ActorSystem has not been initialized");
-                return _actorSystem;
-            }
-        }
-
-        protected CoordinatedShutdown CoordinatedShutdown
-        {
-            get
-            {
-                if (_coordinatedShutdown is null)
-                    throw new Exception("ActorSystem has not been initialized");
-                return _coordinatedShutdown;
-            }
-        }
-
-        protected bool Initialized => _coordinatedShutdown is not null;
-
-        protected async Task StartAkkaAsync(CancellationToken cancellationToken)
-        {
-            _actorSystem = ServiceProvider.GetRequiredService<ActorSystem>();
-            _coordinatedShutdown = CoordinatedShutdown.Get(_actorSystem);
-            await _configurationBuilder.StartAsync(_actorSystem);
-
-            async Task TerminationHook()
-            {
-                await _actorSystem.WhenTerminated.ConfigureAwait(false);
-                HostApplicationLifetime.StopApplication();
-            }
-
-            // terminate the application if the Sys is terminated first
-            // this can happen in instances such as Akka.Cluster membership changes
-#pragma warning disable CS4014
-            TerminationHook();
-#pragma warning restore CS4014
-        }
-
-        protected async Task StopAkkaAsync(CancellationToken cancellationToken)
-        {
-            // ActorSystem may have failed to start - skip shutdown sequence if that's the case
-            // so error message doesn't get conflated.
-            if (!Initialized)
-                return;
-            
-            // run full CoordinatedShutdown on the Sys
-            await CoordinatedShutdown.Run(CoordinatedShutdown.ClrExitReason.Instance)
-                .ConfigureAwait(false);
-        }
-        
-        public virtual async Task StartAsync(CancellationToken cancellationToken)
-        {
-            try
-            {
-                await StartAkkaAsync(cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                Logger.Log(LogLevel.Critical, ex, "Unable to start AkkaHostedService - shutting down application");
-                HostApplicationLifetime.StopApplication();
-            }
-        }
-        
-        public virtual async Task StopAsync(CancellationToken cancellationToken)
-        {
-            await StopAkkaAsync(cancellationToken);
-        }
-    }
-    
     /// <summary>
     /// INTERNAL API
     /// </summary>
-    internal sealed class AkkaHostedServiceImpl : AkkaHostedService
+    internal sealed class AkkaHostedService : IHostedService
     {
-        public AkkaHostedServiceImpl(
-            AkkaConfigurationBuilder configurationBuilder,
-            IServiceProvider serviceProvider,
-            ILogger<AkkaHostedService> logger,
-            IHostApplicationLifetime applicationLifetime)
-        : base(configurationBuilder, serviceProvider, logger, applicationLifetime)
-        { }
+        private ActorSystem? _actorSystem;
+        private CoordinatedShutdown? _coordinatedShutdown; // grab a reference to CoordinatedShutdown early
+        private readonly IServiceProvider _serviceProvider;
+        private readonly AkkaConfigurationBuilder _configurationBuilder;
+        private readonly IHostApplicationLifetime _hostApplicationLifetime;
+        private readonly ILogger<AkkaHostedService> _logger;
+
+        public AkkaHostedService(AkkaConfigurationBuilder configurationBuilder, IServiceProvider serviceProvider,
+            ILogger<AkkaHostedService> logger, IHostApplicationLifetime applicationLifetime)
+        {
+            _configurationBuilder = configurationBuilder;
+            _hostApplicationLifetime = applicationLifetime;
+            _serviceProvider = serviceProvider;
+            _logger = logger;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                _actorSystem = _serviceProvider.GetRequiredService<ActorSystem>();
+                _coordinatedShutdown = CoordinatedShutdown.Get(_actorSystem);
+                await _configurationBuilder.StartAsync(_actorSystem);
+
+                async Task TerminationHook()
+                {
+                    await _actorSystem.WhenTerminated.ConfigureAwait(false);
+                    _hostApplicationLifetime.StopApplication();
+                }
+
+                // terminate the application if the Sys is terminated first
+                // this can happen in instances such as Akka.Cluster membership changes
+#pragma warning disable CS4014
+                TerminationHook();
+#pragma warning restore CS4014
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(LogLevel.Critical, ex, "Unable to start AkkaHostedService - shutting down application");
+                _hostApplicationLifetime.StopApplication();
+            }
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            // ActorSystem may have failed to start - skip shutdown sequence if that's the case
+            // so error message doesn't get conflated.
+            if (_coordinatedShutdown == null)
+            {
+                return;
+            }
+            
+            // run full CoordinatedShutdown on the Sys
+            await _coordinatedShutdown.Run(CoordinatedShutdown.ClrExitReason.Instance)
+                .ConfigureAwait(false);
+        }
     }
 }
