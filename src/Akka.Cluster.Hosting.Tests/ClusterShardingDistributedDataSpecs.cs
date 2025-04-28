8c4dee8a4fd3f00961e37d8eaa90598794173da3
Gregorius Soedharmo <arkatufus@yahoo.com>
2025-04-28 22:34:12 +0700

    `WithDistributedData()` will start `DistributedDataProvider` extension automatically.

---
+++ src/Akka.Cluster.Hosting.Tests/ClusterShardingDistributedDataSpecs.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.DistributedData;
+using Akka.Hosting;
+using Akka.Remote.Hosting;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Akka.Cluster.Hosting.Tests;
+
+public class ClusterShardingDistributedDataSpecs: Akka.Hosting.TestKit.TestKit
+{
+    private const string ReplicatorName = "dDataReplicator";
+    
+    public ClusterShardingDistributedDataSpecs(ITestOutputHelper output): base(output: output)
+    {
+    }
+    
+    protected override void ConfigureAkka(AkkaConfigurationBuilder builder, IServiceProvider provider)
+    {
+        builder
+            .WithRemoting()
+            .WithClustering()
+            .WithDistributedData(opt =>
+            {
+                opt.Name = ReplicatorName;
+            });
+    }
+
+    [Fact(DisplayName = "WithDistributedData should start DistributedData extension automatically")]
+    public async Task WithDistributedDataStartsAutomaticallyTest()
+    {
+        var cluster = Cluster.Get(Sys);
+        cluster.Join(cluster.SelfAddress);
+        
+        var settings = ReplicatorSettings.Create(Sys);
+        var coordinatorName = settings.RestartReplicatorOnFailure ? $"{ReplicatorName}Supervisor" : ReplicatorName;
+        
+        var actorSelection = Sys.ActorSelection(new RootActorPath(cluster.SelfAddress) / "user" / coordinatorName);
+        
+        await AwaitAssertAsync(() =>
+        {
+            actorSelection.Tell(new Identify("coordinator"), TestActor);
+            var identity = ExpectMsg<ActorIdentity>(TimeSpan.FromSeconds(3));
+            
+            // The DData replicator should be running
+            // * This actor is created inside DistributedData extension .ctor
+            // * Marks that the extension is running
+            // * We can't use DistributedData.Get() because that defeats the purpose.
+            identity.Subject.Should().NotBeNull();
+        });
+    }
+}
