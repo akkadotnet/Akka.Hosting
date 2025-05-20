84cf681984c1ae40ca24bd9600b952174362a685
Gregorius Soedharmo <arkatufus@yahoo.com>
2025-05-21 01:35:55 +0700

    Split logging examples to ASP.NET web app and MS.EXT.Hosting console projects (#598)

---
+++ src/Examples/Akka.Hosting.Asp.LoggingDemo/Program.cs
@@ -0,0 +1,56 @@
+using Akka.Hosting;
+using Akka.Actor;
+using Akka.Actor.Dsl;
+using Akka.Cluster.Hosting;
+using Akka.Event;
+using Akka.Hosting.Asp.LoggingDemo;
+using Akka.Remote.Hosting;
+using LogLevel = Akka.Event.LogLevel;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAkka("MyActorSystem", (configurationBuilder, serviceProvider) =>
+{
+    configurationBuilder
+        .ConfigureLoggers(setup =>
+        {
+            // This sets the minimum log level
+            setup.LogLevel = LogLevel.DebugLevel;
+            
+            // Clear all loggers (remove the default console logger)
+            setup.ClearLoggers();
+            
+            // Add the ILoggerFactory logger
+            // NOTE:
+            //   - You can also use setup.AddLogger<LoggerFactoryLogger>();
+            //   - To use a specific ILoggerFactory instance, you can use setup.AddLoggerFactory(myILoggerFactory);
+            setup.AddLoggerFactory();
+        })
+        .WithRemoting("localhost", 8110)
+        .WithClustering(new ClusterOptions { 
+            Roles = ["myRole"], 
+            SeedNodes = ["akka.tcp://MyActorSystem@localhost:8110"]
+        })
+        .WithActors((system, registry) =>
+        {
+            var echo = system.ActorOf(act =>
+            {
+                act.ReceiveAny((o, context) =>
+                {
+                    Logging.GetLogger(context.System, "echo").Info($"Actor received {o}");
+                    context.Sender.Tell($"{context.Self} rcv {o}");
+                });
+            }, "echo");
+            registry.TryRegister<Echo>(echo); // register for DI
+        });
+});
+
+var app = builder.Build();
+
+app.MapGet("/", async (context) =>
+{
+    var echo = context.RequestServices.GetRequiredService<ActorRegistry>().Get<Echo>();
+    var body = await echo.Ask<string>(context.TraceIdentifier, context.RequestAborted).ConfigureAwait(false);
+    await context.Response.WriteAsync(body);
+});
+
+app.Run();
--- src/Examples/Akka.Hosting.LoggingDemo/Program.cs
+++ src/Examples/Akka.Hosting.LoggingDemo/Program.cs
@@ -3,68 +3,55 @@ using Akka.Actor;
 using Akka.Actor.Dsl;
 using Akka.Cluster.Hosting;
 using Akka.Event;
-using Akka.Hosting.Logging;
 using Akka.Hosting.LoggingDemo;
-using Akka.Logger.Serilog;
 using Akka.Remote.Hosting;
-using Serilog;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using LogLevel = Akka.Event.LogLevel;
 
-Serilog.Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console()
-    .MinimumLevel.Debug()
-    .CreateLogger();
+var builder = Host.CreateDefaultBuilder(args);
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddAkka("MyActorSystem", (configurationBuilder, serviceProvider) =>
-{
-    configurationBuilder
-        .ConfigureLoggers(setup =>
-        {
-            // Example: This sets the minimum log level
-            setup.LogLevel = LogLevel.DebugLevel;
-            
-            // Example: Clear all loggers
-            setup.ClearLoggers();
-            
-            // Example: Add the default logger
-            // NOTE: You can also use setup.AddLogger<DefaultLogger>();
-            setup.AddDefaultLogger();
-            
-            // Example: Add the ILoggerFactory logger
-            // NOTE:
-            //   - You can also use setup.AddLogger<LoggerFactoryLogger>();
-            //   - To use a specific ILoggerFactory instance, you can use setup.AddLoggerFactory(myILoggerFactory);
-            setup.AddLoggerFactory();
-            
-            // Example: Adding a serilog logger
-            //setup.AddLogger<SerilogLogger>(); // TODO: add back once upgraded to v1.5 APIs
-        })
-        .WithRemoting("localhost", 8110)
-        .WithClustering(new ClusterOptions(){ Roles = new[]{ "myRole" }, 
-            SeedNodes = new[]{ "akka.tcp://MyActorSystem@localhost:8110" }})
-        .WithActors((system, registry) =>
-        {
-            var echo = system.ActorOf(act =>
+await builder
+    .ConfigureServices(services =>
+    {
+        services
+            .AddSingleton<WorkerService>()
+            .AddHostedService<WorkerService>()
+            .AddAkka("MyActorSystem", (configurationBuilder, serviceProvider) =>
             {
-                act.ReceiveAny((o, context) =>
-                {
-                    Logging.GetLogger(context.System, "echo").Info($"Actor received {o}");
-                    context.Sender.Tell($"{context.Self} rcv {o}");
-                });
-            }, "echo");
-            registry.TryRegister<Echo>(echo); // register for DI
-        });
-});
-
-var app = builder.Build();
-
-app.MapGet("/", async (context) =>
-{
-    var echo = context.RequestServices.GetRequiredService<ActorRegistry>().Get<Echo>();
-    var body = await echo.Ask<string>(context.TraceIdentifier, context.RequestAborted).ConfigureAwait(false);
-    await context.Response.WriteAsync(body);
-});
-
-app.Run();
+                configurationBuilder
+                    .ConfigureLoggers(setup =>
+                    {
+                        // This sets the minimum log level
+                        setup.LogLevel = LogLevel.DebugLevel;
+                    
+                        // Clear all loggers (remove the default console logger)
+                        setup.ClearLoggers();
+                    
+                        // Add the ILoggerFactory logger
+                        // NOTE:
+                        //   - You can also use setup.AddLogger<LoggerFactoryLogger>();
+                        //   - To use a specific ILoggerFactory instance, you can use setup.AddLoggerFactory(myILoggerFactory);
+                        setup.AddLoggerFactory();
+                    })
+                    .WithRemoting("localhost", 8110)
+                    .WithClustering(new ClusterOptions { 
+                        Roles = ["myRole"], 
+                        SeedNodes = ["akka.tcp://MyActorSystem@localhost:8110"]
+                    })
+                    .WithActors((system, registry) =>
+                    {
+                        var echo = system.ActorOf(act =>
+                        {
+                            act.ReceiveAny((o, context) =>
+                            {
+                                Logging.GetLogger(context.System, "echo").Info($"Actor received {o}");
+                                context.Sender.Tell($"{context.Self} rcv {o}");
+                            });
+                        }, "echo");
+                        registry.TryRegister<Echo>(echo); // register for DI
+                    });
+            });
+    })
+    .UseConsoleLifetime()
+    .RunConsoleAsync();
