fd126e06f1261b17efac78f085b810d3f9958249
Aaron Stannard <aaron@petabridge.com>
2025-07-23 11:34:25 -0500

    fleshed out low-level healthcheck APIs

--- src/Akka.Hosting/IAkkaHealthCheck.cs
+++ src/Akka.Hosting/IAkkaHealthCheck.cs
@@ -10,15 +10,99 @@ namespace Akka.Hosting;
 /// <summary>
 /// The registration record used to track this Akka.HealthCheck inside the <see cref="AkkaConfigurationBuilder"/>
 /// </summary>
+/// <remarks>
+/// These will all get converted into <see cref="HealthCheckRegistration"/>s by the <see cref="AkkaConfigurationBuilder"/>,
+/// and a default "akka.net" tag will be added to each of those registrations for filtering purposes.
+/// </remarks>
 public sealed class AkkaHealthCheckRegistration
 {
     private string _name;
     private Func<ActorSystem, IAkkaHealthCheck>  _factory;
+    private TimeSpan _timeout;
+
+    /// <summary>
+    /// Creates a new <see cref="AkkaHealthCheckRegistration"/> for an existing <see cref="IAkkaHealthCheck"/>
+    /// </summary>
+    /// <param name="name">The healthcheck name.</param>
+    /// <param name="instance">The <see cref="IAkkaHealthCheck"/> instance.</param>
+    /// <param name="failureStatus">
+    /// The <see cref="HealthStatus"/> that should be reported upon failure of the health check. If the provided value
+    /// is <c>null</c>, then <see cref="HealthStatus.Unhealthy"/> will be reported.
+    /// </param>
+    /// <param name="tags">A list of tags that can be used for filtering health checks.</param>
+    public AkkaHealthCheckRegistration(string name, IAkkaHealthCheck instance, HealthStatus? failureStatus,
+        IEnumerable<string>? tags)
+        : this(name, instance, failureStatus, tags, default)
+    {
+        
+    }
+
+    /// <summary>
+    /// Creates a new <see cref="AkkaHealthCheckRegistration"/> for an existing <see cref="IAkkaHealthCheck"/>
+    /// </summary>
+    /// <param name="name">The healthcheck name.</param>
+    /// <param name="instance">The <see cref="IAkkaHealthCheck"/> instance.</param>
+    /// <param name="failureStatus">
+    /// The <see cref="HealthStatus"/> that should be reported upon failure of the health check. If the provided value
+    /// is <c>null</c>, then <see cref="HealthStatus.Unhealthy"/> will be reported.
+    /// </param>
+    /// <param name="tags">A list of tags that can be used for filtering health checks.</param>
+    /// <param name="timeout">An optional <see cref="TimeSpan"/> representing the timeout of the check.</param>
+    /// <exception cref="ArgumentNullException">Thrown if <see cref="name"/> or <see cref="instance"/> is null.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if a negative timeout other than <see cref="System.Threading.Timeout.InfiniteTimeSpan"/> is used.</exception>
+    public AkkaHealthCheckRegistration(string name, IAkkaHealthCheck instance, HealthStatus? failureStatus,
+        IEnumerable<string>? tags, TimeSpan? timeout)
+    {
+        if(name == null)  throw new ArgumentNullException(nameof(name));
+        if(instance == null) throw new ArgumentNullException(nameof(instance));
+
+        if (timeout <= TimeSpan.Zero && timeout != System.Threading.Timeout.InfiniteTimeSpan)
+        {
+            throw new ArgumentOutOfRangeException(nameof(timeout));
+        }
+        
+        _name = name;
+        FailureStatus = failureStatus ?? HealthStatus.Unhealthy;
+        Tags = new HashSet<string>(tags ?? [], StringComparer.OrdinalIgnoreCase);
+        _factory =  (_) => instance;
+        Timeout = timeout ?? System.Threading.Timeout.InfiniteTimeSpan;
+    }
+    
+    /// <summary>
+    /// Creates a new <see cref="AkkaHealthCheckRegistration"/> for an existing <see cref="IAkkaHealthCheck"/>
+    /// </summary>
+    /// <param name="name">The healthcheck name.</param>
+    /// <param name="factory">A delegate that produces an <see cref="IAkkaHealthCheck"/> instance from the <see cref="ActorSystem"/>.</param>
+    /// <param name="failureStatus">
+    /// The <see cref="HealthStatus"/> that should be reported upon failure of the health check. If the provided value
+    /// is <c>null</c>, then <see cref="HealthStatus.Unhealthy"/> will be reported.
+    /// </param>
+    /// <param name="tags">A list of tags that can be used for filtering health checks.</param>
+    /// <param name="timeout">An optional <see cref="TimeSpan"/> representing the timeout of the check.</param>
+    /// <exception cref="ArgumentNullException">Thrown if <see cref="name"/> or <see cref="factory"/> is null.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if a negative timeout other than <see cref="System.Threading.Timeout.InfiniteTimeSpan"/> is used.</exception>
+    public AkkaHealthCheckRegistration(string name, Func<ActorSystem, IAkkaHealthCheck> factory, HealthStatus? failureStatus,
+        IEnumerable<string>? tags, TimeSpan? timeout)
+    {
+        if(name == null)  throw new ArgumentNullException(nameof(name));
+        if(factory == null) throw new ArgumentNullException(nameof(factory));
+
+        if (timeout <= TimeSpan.Zero && timeout != System.Threading.Timeout.InfiniteTimeSpan)
+        {
+            throw new ArgumentOutOfRangeException(nameof(timeout));
+        }
+        
+        _name = name;
+        FailureStatus = failureStatus ?? HealthStatus.Unhealthy;
+        Tags = new HashSet<string>(tags ?? [], StringComparer.OrdinalIgnoreCase);
+        _factory = factory;
+        Timeout = timeout ?? System.Threading.Timeout.InfiniteTimeSpan;
+    }
     
     /// <summary>
     /// Gets or sets the delay applied to the health check after the application starts up.
     ///
-    /// THe delay is applied once at startup and does not apply to subsequent iterations.
+    /// The delay is applied once at startup and does not apply to subsequent iterations.
     /// </summary>
     public TimeSpan? Delay { get; set; }
     
@@ -47,6 +131,42 @@ public sealed class AkkaHealthCheckRegistration
         get => _factory;
         set => _factory = value ?? throw new ArgumentNullException(nameof(value));
     }
+    
+    /// <summary>
+    /// Gets or sets the <see cref="HealthStatus"/> that should be reported upon failure of the health check.
+    /// </summary>
+    public HealthStatus FailureStatus { get; set; }
+
+    /// <summary>
+    /// Gets or sets the timeout used for the test.
+    /// </summary>
+    public TimeSpan Timeout
+    {
+        get => _timeout;
+        set
+        {
+            if (value <= TimeSpan.Zero && value != System.Threading.Timeout.InfiniteTimeSpan)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value));
+            }
+
+            _timeout = value;
+        }
+    }
+}
+
+/// <summary>
+/// Health check context. Provides health check registrations to <see cref="IAkkaHealthCheck.CheckHealthAsync(AkkaHealthCheckContext, System.Threading.CancellationToken)"/>
+/// </summary>
+public sealed class AkkaHealthCheckContext
+{
+    /// <summary>
+    /// Gets or sets the <see cref="AkkaHealthCheckRegistration"/> of the currently executing <see cref="IAkkaHealthCheck"/>.
+    /// </summary>
+    /// <remarks>
+    /// This allows null values for convenience during unit testing. This is expected to be non-null when within application code.
+    /// </remarks>
+    public AkkaHealthCheckRegistration Registration { get; set; } = default!;
 }
 
 /// <summary>
@@ -54,5 +174,5 @@ public sealed class AkkaHealthCheckRegistration
 /// </summary>
 public interface IAkkaHealthCheck
 {
-    Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default);
+    Task<HealthCheckResult> CheckHealthAsync(AkkaHealthCheckContext context, CancellationToken cancellationToken = default);
 }
